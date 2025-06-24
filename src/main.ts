1e891ad854fc97c0b683a48011a5f9f134309b31
pallascodes <pallascodes@gmail.com>
2025-06-24 13:26:56 -0600

    refactor: remove unused AppController and AppService, update main.ts logging

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { Logger } from '@nestjs/common'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+
+  const logger = new Logger()
+  const PORT = process.env.PORT ?? 3000
+
+  await app.listen(PORT)
+  logger.log(`App running on port ${PORT}`)
 }
-bootstrap();
+bootstrap()
